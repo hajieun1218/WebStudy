fcee232835146c9a7beed946e966dc3078160c5a
hajieun1218 <gkwldms1218@naver.com>
2020-03-13 15:25:25 +0900

    2020-03-13

---
+++ JSPBasicProject5/src/com/sist/temp/Sawon.java
@@ -0,0 +1,15 @@
+package com.sist.temp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor  // 매개변수 들어간 생성자
+public class Sawon {
+	private int sabun;
+	private String name;
+	private String dept;
+	
+}
